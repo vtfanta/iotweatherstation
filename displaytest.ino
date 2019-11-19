d21b82fb6a859153e7f4ae82ebee9f340fb0127f
fantavit <fantavit@fel.cvut.cz>
2019-11-19 21:32:54 +0100

    added sensor functionality with display

--- displaytest.ino
+++ displaytest.ino
@@ -1,14 +1,32 @@
- This #include statement was automatically added by the Particle IDE.
-#include Adafruit_SSD1306.h
-#include Adafruit_GFX.h
+// This #include statement was automatically added by the Particle IDE.
+#include <PietteTech_DHT.h>
 
-  Uncomment this block to use hardware SPI
- If using software SPI (the default case)
+// This #include statement was automatically added by the Particle IDE.
+#include <Adafruit_SSD1306.h>
+#include "Adafruit_GFX.h"
+
+// /* Uncomment this block to use hardware SPI
+// If using software SPI (the default case):
 #define OLED_MOSI   D0
 #define OLED_CLK    D1
 #define OLED_DC     D2
 #define OLED_CS     D3
 #define OLED_RESET  D4
+
+#define DHTTYPE  DHT11              // Sensor type DHT11/21/22/AM2301/AM2302
+#define DHTPIN   D6           	    // Digital pin for communications
+#define DHT_SAMPLE_INTERVAL   2000  // Sample every two seconds
+
+ //declaration
+void dht_wrapper(); // must be declared before the lib initialization
+
+// Lib instantiate
+PietteTech_DHT DHT(DHTPIN, DHTTYPE, dht_wrapper);
+
+// globals
+unsigned int DHTnextSampleTime;	    // Next time we want to start sample
+bool bDHTstarted;		    // flag to indicate we started acquisition
+
 Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
 char incoming;
@@ -17,37 +35,43 @@ void setup() {
     display.begin(SSD1306_SWITCHCAPVCC);
     Serial.begin(9600);
     
+    DHT.begin();
+
+    DHTnextSampleTime = 0;  // Start the first sample immediately
+    
+    display.clearDisplay();
+}
+
+// This wrapper is in charge of calling
+ // must be defined like this for the lib work
+void dht_wrapper() {
+  DHT.isrCallback();
 }
 
 void loop() {
-     Reads a string from the serial input and prints it on the screen for 2 seconds
-    display.clearDisplay();
-    display.display();
     display.setCursor(0,0);
     display.setTextSize(1);
     display.setTextColor(WHITE);
-    Serial.print(Hellorn);
+    DHT.acquire();
+    
+    if (!DHT.acquiring()) {
+        display.clearDisplay();
+        display.display();
    
-   
-    int len = Serial.available();
-    Serial.print(len);
-    if (len  0) {
-        char incoming = (char )malloc(len  sizeof(char)+1);
-        for (int i = 0; i  len; i++) {
-            incoming[i] = Serial.read();
-        }
-        incoming[len] = '0';
+        String s1 = String("Temp: ");
+        String sT = String(DHT.getCelsius(), 0);
+        s1.concat(sT);
+        s1.concat(String(" C\r\n"));
         
-        display.println(incoming);
-        free(incoming);
-    } else {
-        display.println(Sadface...);    
+        String s2 = String("Hum: ");
+        String sH = String(DHT.getHumidity(), 0);
+        s2.concat(sH);
+        s2.concat(String(" %\r\n"));
+        
+        display.println(s1);
+        display.println(s2);
+        
+        display.display();
+        delay(DHT_SAMPLE_INTERVAL);
     }
-    display.display();
-    delay(2000);
-    display.clearDisplay();
-     display.invertDisplay(true);
-     delay(2000);
-     display.invertDisplay(false);
-     delay(2000);
 }
